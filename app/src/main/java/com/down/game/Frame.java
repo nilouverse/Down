c08e58ca84008d58d13d23191cfa27a6a6e488a8
Nilouverse Studios <aiko.literature@gmail.com>
2026-08-17 23:42:37 +0330

    Create Frame.java

---
+++ app/src/main/java/com/down/game/Frame.java
@@ -0,0 +1,10 @@
+package com.down.game;
+
+import android.graphics.Bitmap;
+
+public class Frame {
+    public Bitmap bmp;
+    public int top, ch, left, cw, rgt, ww;
+    public float ref, dx;
+    public boolean vCrop, cCenter;
+}
